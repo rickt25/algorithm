aa6cb34507a3e9cd8ae621ce3acbbb9d0908ab71
Derrick <dt@M1-Macbook-Air.local>
2021-04-13 11:28:44 +0700

    hashtable

--- Data-structures/hashtable.cpp
+++ Data-structures/hashtable.cpp
@@ -3,4 +3,52 @@
 #include <string.h>
 
 #define HASHSIZE 20
-// test
+struct Node{
+    char name[25];
+    struct Node* next;
+};
+
+Node* hashTable[HASHSIZE];
+
+Node* createNode(const char* name){
+    Node* temp = (Node*)malloc(sizeof(Node));
+    strcpy(temp->name,name);
+    temp->next = NULL;
+    return temp;
+}
+
+int Hash(const char* name){
+    int count;
+    for(int i=0; i<strlen(name); i++)
+        count += name[i];
+    return count%HASHSIZE;
+}
+
+void Insert(const char* name){
+    Node* temp = createNode(name);
+    int hashIdx = Hash(name);
+
+    if(hashTable[hashIdx] != NULL)
+        temp->next = hashTable[hashIdx];
+    
+    hashTable[hashIdx] = temp;
+    return;
+}
+
+void Print(){
+    for(int i=0; i<=HASHSIZE; i++){
+        Node* curr = hashTable[i];
+        while(curr){
+            printf("%s\n", curr->name);
+            curr = curr->next;
+        }
+    }
+}
+
+
+int main(){
+    Insert("Budi");
+    Insert("Angga");
+    Insert("Caonima");
+    Print();
+}
