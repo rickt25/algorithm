b3754b1a103bed1a379cabea9664920ecdfa922b
Derrick <dt@M1-Macbook-Air.local>
2021-04-17 22:21:43 +0700

    finish hashtable

--- Data-structures/hashtable.cpp
+++ Data-structures/hashtable.cpp
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define HASHSIZE 20
+#define SIZE 10
 struct Node{
     char name[25];
-    struct Node* next;
+    Node *next;
 };
 
-Node* hashTable[HASHSIZE];
+Node *head[SIZE], *tail[SIZE];
 
 Node* createNode(const char* name){
     Node* temp = (Node*)malloc(sizeof(Node));
@@ -18,37 +18,92 @@ Node* createNode(const char* name){
 }
 
 int Hash(const char* name){
-    int count;
+    int count = 0;
     for(int i=0; i<strlen(name); i++)
         count += name[i];
-    return count%HASHSIZE;
+    return count%SIZE;
+}
+
+Node* Search(const char* name){
+    int idx = Hash(name);
+    Node* curr = head[idx];
+    while(curr){
+        if(strcmp(curr->name,name) == 0)
+            return curr;
+        curr = curr->next; 
+    }
+    return NULL;
 }
 
 void Insert(const char* name){
     Node* temp = createNode(name);
-    int hashIdx = Hash(name);
+    int idx = Hash(name);
 
-    if(hashTable[hashIdx] != NULL)
-        temp->next = hashTable[hashIdx];
-    
-    hashTable[hashIdx] = temp;
-    return;
+    if(!head[idx])
+        head[idx] = tail[idx] = temp;
+    else{
+        tail[idx]->next = temp;
+        tail[idx] = temp;
+    }
+}
+
+void Update(const char* name, const char* edit){
+    Node* temp = Search(name);
+    if(temp) strcpy(temp->name, edit);
+    else printf("%s not found\n", name);
+}
+
+void Delete(const char* name){
+    int idx = Hash(name);
+    Node *curr = head[idx], *prev;
+    while(curr){
+        if(strcmp(curr->name,name) == 0){
+            if(curr == head[idx]){
+                if(!curr->next){
+                    head[idx] = tail[idx] = head[idx]->next = NULL;
+                    free(curr);
+                }else{
+                    Node* temp = curr->next;
+                    head[idx]->next = NULL;
+                    free(head[idx]);
+                    head[idx] = temp;
+                }
+            }else if(curr == tail[idx]){
+                prev->next = NULL;
+                free(tail[idx]);
+                tail[idx] = prev;
+            }else{
+                Node* temp = curr->next;
+                curr->next = NULL;
+                free(curr);
+                prev->next = temp;
+            }
+        }
+        prev = curr;
+        curr = curr->next;
+    }
 }
 
 void Print(){
-    for(int i=0; i<=HASHSIZE; i++){
-        Node* curr = hashTable[i];
+    for(int i=0; i<=SIZE; i++){
+        printf("%d. ",i);
+        Node* curr = head[i];
         while(curr){
-            printf("%s\n", curr->name);
+            if(curr == head[i]) printf("(head)");
+            if(curr == tail[i]) printf("(tail)");
+            printf("%s -> ", curr->name);
             curr = curr->next;
         }
+        puts("");
     }
 }
 
 
 int main(){
     Insert("Budi");
-    Insert("Angga");
-    Insert("Caonima");
+    Insert("Anton");
+    Insert("Kucing");
+    Update("Budi","Badu");
+    Delete("Kucing");
     Print();
 }
